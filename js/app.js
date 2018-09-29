247ca00c5bd1f8c24c98f87c5f7fa2b9466cc957
AJAEGBU SHEDRACK IWUCHUkWU <shedrack3@gmail.com>
2018-09-29 04:16:45 +0100

    removed the functionality for touch screen - was not working

--- js/app.js
+++ js/app.js
@@ -70,56 +70,6 @@ Player.prototype.handleInput = function (input) {
     }
 }
 
-// touch event for the player object
-Player.prototype.swipedetect = function (el, callback) {
-
-    var touchsurface = el,
-        swipedir,
-        startX,
-        startY,
-        distX,
-        distY,
-        threshold = 101, //required min distance traveled to be considered swipe
-        restraint = 83, // maximum distance allowed at the same time in perpendicular direction
-        allowedTime = 200, // maximum time allowed to travel that distance
-        elapsedTime,
-        startTime,
-        handleswipe = callback || function (swipedir) {}
-
-    // Listens for touch start
-    touchsurface.addEventListener('touchstart', function (e) {
-        var touchobj = e.changedTouches[0]
-        swipedir = 'none'
-        dist = 0
-        startX = touchobj.pageX
-        startY = touchobj.pageY
-        startTime = new Date().getTime() // record time when finger first makes contact with surface
-        e.preventDefault()
-    }, false)
-
-    // Prevent the touchmove event default to avoid page scrolling.
-    touchsurface.addEventListener('touchmove', function (e) {
-        e.preventDefault() // prevent scrolling when inside DIV
-    }, false)
-
-    // Listens for the touch end
-    touchsurface.addEventListener('touchend', function (e) {
-        var touchobj = e.changedTouches[0]
-        distX = touchobj.pageX - startX // get horizontal dist traveled by finger while in contact with surface
-        distY = touchobj.pageY - startY // get vertical dist traveled by finger while in contact with surface
-        elapsedTime = new Date().getTime() - startTime // get time elapsed
-        if (elapsedTime <= allowedTime) { // first condition for awipe met
-            if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint) { // 2nd condition for horizontal swipe met
-                swipedir = (distX < 0) ? 'left' : 'right' // if dist traveled is negative, it indicates left swipe
-            } else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint) { // 2nd condition for vertical swipe met
-                swipedir = (distY < 0) ? 'up' : 'down' // if dist traveled is negative, it indicates up swipe
-            }
-        }
-        handleswipe(swipedir)
-        e.preventDefault()
-    }, false)
-}
-
 //added update method to the player object
 Player.prototype.update = function () {
     // loop through all the enemies and check for collision and win
